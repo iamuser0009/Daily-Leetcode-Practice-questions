82b0a97896cf3dda4e3b44c6ba3a4eb428bfbe66
iamuser0009 <2k23.csds2311091@gmail.com>
2026-07-03 23:35:06 +0530

    Time: 0 ms (100%), Space: 42 MB (51.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,16 @@
 class Solution {
+    int[] dp ;
     public int fib(int n) {
-        if( n==0) return 0;
-        if(n ==1) return 1;
-        return fib(n-1)+fib(n-2);
+     dp = new int[n+1];
+    return dynamic(n);
         
+    }
+    public int dynamic(int  n){
+        if(n<=1) return n;
+        if(dp[n] != 0) return dp[n];
+        else{
+            dp[n] = dynamic(n-1) +dynamic(n-2);
+        }
+        return dp[n];
     }
 }
