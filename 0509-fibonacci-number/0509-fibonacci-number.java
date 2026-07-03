fdcf6a261cb0683f8dc30cf7c2db1fe75068b70a
iamuser0009 <2k23.csds2311091@gmail.com>
2026-07-03 23:37:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (51.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -8,9 +8,9 @@ class Solution {
     public int dynamic(int  n){
         if(n<=1) return n;
         if(dp[n] != 0) return dp[n];
-        else{
+        // else{
             dp[n] = dynamic(n-1) +dynamic(n-2);
-        }
+        // }
         return dp[n];
     }
 }
